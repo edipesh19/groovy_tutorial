3ecf8965fab446276fac5974c94c87875762e3e2
Dipesh Dutta <dipesh.dutta@oracle.com>
2019-09-30 23:41:38 +0530

    IO demonstration

---
+++ src/14InoutOutput.groovy
@@ -0,0 +1,17 @@
+print 'Enter your name: '
+def name = System.console().readLine()
+println "Hello $name"
+
+print 'Enter first  number: '
+def num1 = System.console().readLine().toInteger()
+
+print 'Enter second  number: '
+def num2 = System.console().readLine().toInteger()
+
+println "$num1 + $num2 = " + (num1 + num2)
+
+
+// Output
+//print()
+//println()
+printf() // Similar to c printf
